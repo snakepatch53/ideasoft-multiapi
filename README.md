5dc89b8d018165184dfedb5804def3a350db7c8b
snakepatch53 <snakepatch53@gmail.com>
2025-10-03 18:39:30 -0500

    feat: actualizar instrucciones de instalación en el README y mejorar la lógica de mensajes en las rutas

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 Primero descargar e instalar localmente Ollama
 
 https://ollama.com/download
+
+luego ejecutar en terminal para descargar el modelo:
+
+ollama pull gemma3
--- start/routes.ts
+++ start/routes.ts
@@ -14,8 +14,18 @@ router.get('/ollama', async function ({ inertia }) {
 router.post('/message', async function ({ request, response }) {
     const userMessage = request.input('message');
     messages.push({ role: 'user', content: userMessage });
-    const ollamaResponse = await ollama.chat({ model: 'gemma3', messages });
-    messages.push({ role: 'assistant', content: ollamaResponse.message.content });
+    // const ollamaResponse = await ollama.chat({ model: 'gemma3', messages });
+    const ollamaResponse = await ollama.chat({
+        model: 'gpt-oss:120b-cloud',
+        messages: [{ role: 'user', content: 'Explain quantum computing' }],
+        stream: true,
+    });
+
+    let messageContent = '';
+    for await (const part of ollamaResponse) {
+        if (part.message?.content) messageContent += part.message.content;
+    }
+    messages.push({ role: 'assistant', content: messageContent });
     return response.redirect().back();
 });
 
